7e11477161f1eafbdb914804b898f0f48709dcec
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-11-09 13:06:22 +0100

    vamos a comprobar si funciona el placeholder de imagen vacia

--- middlewares/validRoute.ts
+++ middlewares/validRoute.ts
@@ -7,10 +7,13 @@ import path from 'path';
 const validRoute = (req:Request,res:Response,next:NextFunction) => {
     const { ruta } = req.params;
     const pathimg = path.join(__dirname,'../db&storage/storage',ruta);
+    /*
     if(fs.existsSync(pathimg)){req.body.ruta = pathimg ; next()}else{
         req.body.ruta = path.join(__dirname,'../db&storage/storage/assets/inf.png');
         next();
     };
+    */
+   req.body.ruta = path.join(__dirname,'../db&storage/storage/assets/inf.png') ; next();
 }
 
 module.exports = { validRoute }
