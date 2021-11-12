11d9fef731a406052e161d269c7d74259ee04e6d
Carlos Fraile <carlosfrailegomez93@gmail.com>
2021-11-12 15:44:13 +0100

    buen achucheido

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map , tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  info!:pics[];
+  baseURL:string = "http://localhost:8000/api/crudimg";
+  getpics=this._hc.get(this.baseURL).pipe(
+    map<any,pics[]>(resp => {
+      resp.forEach((x:any) => { x.id = x._id ; delete x._id ; delete x.__v ; x.funa = false ;});
+      return resp;
+    }),
+    tap(console.log)
+  )
+
+  constructor( private _hc:HttpClient ){
+    this.getpics.subscribe(resp => this.info = resp);
+  }
+
+  funarse(foto:pics){
+    if(foto.funa){
+      this._hc.delete(`${this.baseURL}/${foto.id}`).subscribe(() => {},() => {}, () => {
+        this.getpics.subscribe(resp => this.info = resp);
+      });return;
+    }else{
+      this.info.forEach(x => {x.funa = false});
+      foto.funa = true;
+    }
+  }
+
 }
+
+interface pics {id:string,ruta:string,funa:boolean}
